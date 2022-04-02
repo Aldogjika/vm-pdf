f38dd32c995edbc49ad80662db54c3b163d07acf
Aldo Gjika <gjikaaldo@gmail.com>
2022-04-02 19:25:29 +0200

    start app

---
+++ build/precache-manifest.23acbd3960c35b715f5a009c17472cca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b35adb0bbea4229ebc685d1dfb3fc8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2279bdd51d587ba7ee68",
+    "url": "/static/css/main.ff8df5dc.chunk.css"
+  },
+  {
+    "revision": "9aa869023e5b6ecd3c36",
+    "url": "/static/js/2.d4f96d8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4f96d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2279bdd51d587ba7ee68",
+    "url": "/static/js/main.1d99255e.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  },
+  {
+    "revision": "8562b6565f5ae1db5e4af40d85b4ed2d",
+    "url": "/static/media/Netflix_Logo_RGB.8562b656.png"
+  }
+]);
